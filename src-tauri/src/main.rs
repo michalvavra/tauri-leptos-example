f75b64a5612f3938bf571a4a72232ebe8d1b8234
Michal Vavra <michal@vavra.tech>
2022-12-15 00:02:50 +0100

    feat: add Tauri event example

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,25 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{Manager, Window};
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    message: String,
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn greet(window: Window, name: &str) -> String {
+    window
+        .emit_all(
+            "custom-event",
+            Payload {
+                message: "This is message from custom Tauri event.".into(),
+            },
+        )
+        .unwrap();
+
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
--- src-ui/src/lib.rs
+++ src-ui/src/lib.rs
@@ -1,43 +1,75 @@
 use leptos::*;
 use serde::{Deserialize, Serialize};
-use serde_wasm_bindgen::to_value;
-use wasm_bindgen::prelude::*;
+use serde_wasm_bindgen::{from_value, to_value};
+use wasm_bindgen::{convert::FromWasmAbi, prelude::*};
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "shell"])]
+    pub async fn open(path: &str, open_with: Option<&str>);
+
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "event"], catch)]
+    pub fn listen(event_name: &str, cb: &Closure<dyn FnMut(JsValue)>) -> Result<JsValue, JsValue>;
 }
 
 #[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
+struct GreetCmdArgs {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct GreetPayload {
+    message: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct TauriEvent {
+    event: String,
+    payload: GreetPayload,
+}
+
+fn tauri_listen(event_name: &str, cb: impl FnMut(JsValue) + 'static) {
+    let cb = Closure::wrap(Box::new(cb) as Box<dyn FnMut(JsValue)>);
+    let _ = listen(event_name, &cb);
+    cb.forget();
+}
+
+async fn greet(name: String) -> String {
+    let res = invoke("greet", to_value(&GreetCmdArgs { name }).unwrap()).await;
+    let msg = res.as_string().unwrap();
+
+    msg
 }
 
 #[component]
 pub fn SimpleCounter(cx: Scope, name: String) -> Element {
     let (value, set_value) = create_signal(cx, 0);
+    let (event_msg, set_event_msg) =
+        create_signal(cx, "No `event` message from Tauri.".to_string());
 
-    let message = create_local_resource(
-        cx,
-        move || name.clone(),
-        move |name| async move {
-            let new_msg = invoke("greet", to_value(&GreetArgs { name: &name }).unwrap()).await;
+    let message = create_local_resource(cx, move || name.to_owned(), |name| greet(name));
+    let tauri_event_listener = create_memo(cx, move |_| {
+        tauri_listen("custom-event", move |e| {
+            log::debug!("Recieved event {:#?}", e);
 
-            let new_msg = &new_msg.as_string().unwrap();
-            log::info!("{}", &new_msg);
+            let tauri_event: TauriEvent = from_value(e).unwrap();
+            set_event_msg(tauri_event.payload.message);
+        });
+    });
 
-            new_msg.clone()
-        },
-    );
+    create_effect(cx, move |_| tauri_event_listener);
 
     view! { cx,
         <div>
-            <button on:click=move |_| set_value(0)>"Clear yeah"</button>
+            <button on:click=move |_| set_value(0)>"Clear"</button>
             <button on:click=move |_| set_value.update(|value| *value -= 1)>"-1"</button>
             <span>"Value: " {move || value().to_string()} "!"</span>
             <button on:click=move |_| set_value.update(|value| *value += 1)>"+1"</button>
             <p>{message}</p>
+            <p>{event_msg}</p>
         </div>
     }
 }
