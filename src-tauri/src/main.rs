faa5f102b36471f605b02393fa0f0c27845e5523
Michal Vavra <michal@vavra.tech>
2022-12-17 17:41:57 +0100

    feat: add `event::once` and infinite `event::listen` example

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,18 +6,26 @@
 use tauri::{Manager, Window};
 
 #[derive(Clone, serde::Serialize)]
-struct Payload {
+struct GreetEvent {
+    greeting: String,
+}
+
+#[derive(Clone, serde::Serialize)]
+struct GenericEvent {
+    num: u16,
     message: String,
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(window: Window, name: &str) -> String {
+    // Events should be emitted from `Window` object only.
+    // `tauri-sys` expects window name to be part of event payload.
     window
         .emit_all(
-            "custom-event",
-            Payload {
-                message: "This is message from custom Tauri event.".into(),
+            "greet-event",
+            GreetEvent {
+                greeting: format!("Hey {}, this message has been sent from Tauri event.", name),
             },
         )
         .unwrap();
@@ -25,9 +33,22 @@ fn greet(window: Window, name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn emit_event(window: Window, num: u16) {
+    window
+        .emit_all(
+            "generic-event",
+            GenericEvent {
+                num,
+                message: format!("Generic event with number {}", num),
+            },
+        )
+        .unwrap();
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, emit_event])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-ui/src/lib.rs
+++ src-ui/src/lib.rs
@@ -3,13 +3,24 @@ use leptos::*;
 use serde::{Deserialize, Serialize};
 use tauri_sys::{event, tauri};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
 struct GreetCmdArgs {
     name: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct GreetPayload {
+#[derive(Serialize)]
+struct EmitEventCmdArgs {
+    num: u16,
+}
+
+#[derive(Debug, Deserialize)]
+struct GreetEventRes {
+    greeting: String,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Deserialize)]
+struct GenericEventRes {
+    num: u16,
     message: String,
 }
 
@@ -19,26 +30,60 @@ async fn greet(name: String) -> String {
         .unwrap()
 }
 
-async fn listen_on_event() -> String {
-    let mut events = event::listen::<GreetPayload>("custom-event").await.unwrap();
-    let event = events.next().await.unwrap();
-    log::debug!("Received event {:#?}", event);
-    event.payload.message
+async fn listen_on_greet_event() -> String {
+    let event = event::once::<GreetEventRes>("greet-event").await.unwrap();
+    log::debug!("Received greet-event {:#?}", event);
+    event.payload.greeting
+}
+
+async fn emit_generic_event(num: u16) {
+    tauri::invoke::<_, ()>("emit_event", &EmitEventCmdArgs { num })
+        .await
+        .unwrap();
+}
+
+async fn listen_on_generic_event(event_writer: WriteSignal<Vec<GenericEventRes>>) {
+    let mut events = event::listen::<GenericEventRes>("generic-event")
+        .await
+        .unwrap();
+
+    while let Some(event) = events.next().await {
+        log::debug!("Received generic-event {:#?}", event);
+        event_writer.update(|all_events| all_events.push(event.payload));
+    }
 }
 
 #[component]
 pub fn SimpleCounter(cx: Scope, name: String) -> Element {
     let (value, set_value) = create_signal(cx, 0);
-    let (event_msg, set_event_msg) = create_signal(cx, "No `event` from Tauri.".to_string());
 
-    let event_res = create_local_resource(cx, move || (), |_| listen_on_event());
+    // Greet event, will clean-up once event is recieved.
+    let (greet_event_msg, set_greet_event_msg) =
+        create_signal(cx, "No `greet-event` from Tauri.".to_string());
 
-    let event_msg_memo = create_memo(cx, move |_| {
-        set_event_msg(event_res.read().unwrap_or_default());
+    let greet_event_resource = create_local_resource(cx, move || (), |_| listen_on_greet_event());
+    let greet_event_msg_memo = create_memo(cx, move |_| {
+        set_greet_event_msg(
+            greet_event_resource
+                .read()
+                .unwrap_or("Waiting for `greet-event` from Tauri".to_string()),
+        );
     });
+    create_effect(cx, move |_| greet_event_msg_memo);
 
-    create_effect(cx, move |_| event_msg_memo);
+    // Generic event, listening constantly.
+    let (event_counter, set_event_counter) = create_signal(cx, 1u16);
+    let (event_vec, set_event_vec) = create_signal::<Vec<GenericEventRes>>(cx, vec![]);
+    let emit_event_action = create_action(cx, |num: &u16| emit_generic_event(num.clone()));
+    create_local_resource(
+        cx,
+        move || set_event_vec,
+        |setter| listen_on_generic_event(setter),
+    );
 
+    // Greet command response.
+    // `greet` commands emits `greet-event`. It has to be called after `listen_on_greet_event`.
+    // (In order to make sure the once event has been hooked up.)
     let msg = create_local_resource(cx, move || name.to_owned(), |name| greet(name));
 
     view! { cx,
@@ -47,8 +92,25 @@ pub fn SimpleCounter(cx: Scope, name: String) -> Element {
             <button on:click=move |_| set_value.update(|value| *value -= 1)>"-1"</button>
             <span>"Value: " {move || value().to_string()} "!"</span>
             <button on:click=move |_| set_value.update(|value| *value += 1)>"+1"</button>
+
             <p>{msg}</p>
-            <p>{event_msg}</p>
+            <p>{greet_event_msg}</p>
+
+            <button on:click=move |_| {
+                emit_event_action.dispatch(event_counter());
+                set_event_counter(event_counter() + 1);
+            }>"Emit generic event"</button>
+
+            <ul>
+                <For each=event_vec key=|e| e.num>
+                    {|cx: Scope, e: &GenericEventRes| {
+                        view! {
+                            cx,
+                            <li>{e.message.clone()}</li>
+                        }
+                    }}
+                </For>
+            </ul>
         </div>
     }
 }
